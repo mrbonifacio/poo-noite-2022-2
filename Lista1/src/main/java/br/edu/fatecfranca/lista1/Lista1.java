a842d0866c7d6c82befdde998439cf74f8d22192
Vinícius Bonifácio Pereira <88351484+mrbonifacio@users.noreply.github.com>
2022-08-19 22:15:17 -0300

    Add files via upload

--- Lista1/src/main/java/br/edu/fatecfranca/lista1/Lista1.java
+++ Lista1/src/main/java/br/edu/fatecfranca/lista1/Lista1.java
@@ -5,6 +5,8 @@
 
 package br.edu.fatecfranca.lista1;
 
+import br.edu.fatecfranca.lista1.exe0.Produto;
+
 /**
  *
  * @author 1090482113002
@@ -23,8 +25,10 @@ public class Lista1 {
                 
                 obj1.comprar(10);
                 obj1.vender(5);
+                obj1.subir(50);
+                obj1.abaixar(25);
+                obj1.mostrar();
                 
-                System.out.println(" Nome: " + obj1.nome + " Qtde: " + obj1.qtde + " Preço " + obj1.preco);
                              
                 //vamos instanviar explorando o metodo construtor
                 //metodo que tem o mesmo nome da classe
@@ -33,15 +37,18 @@ public class Lista1 {
                 Produto obj2 = new Produto ("Bola de basquete", 10, 93.90);          
                 obj2.comprar(3);
                 obj1.vender(2);
-                System.out.println(" Nome: " + obj2.nome + " Qtde: " + obj2.qtde + " Preço " + obj2.preco);
+                obj2.subir(50);
+                obj2.abaixar(25);
+                obj2.mostrar();
                 
                 
                 
                 Produto obj3 = new Produto ("Fortnite", 1, 39.90);         
                 obj3.comprar(1);
                 obj3.vender(3);
-                System.out.println(" Nome: " + obj3.nome + " Qtde: " + obj3.qtde + " Preço " + obj3.preco);
-                
+                obj3.subir(50);
+                obj3.abaixar(25);
+                obj3.mostrar();
                 
     }
 }
